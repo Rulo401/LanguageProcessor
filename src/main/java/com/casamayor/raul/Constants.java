45ab37facd736f4740d8e4d6b51c38a68cf2f908
CASAMAYOR NAVAS, RAUL <raul.casamayor.navas@alumnos.upm.es>
2022-10-05 10:25:21 +0200

    Token codes defined

---
+++ src/main/java/com/casamayor/raul/Constants.java
@@ -0,0 +1,30 @@
+package com.casamayor.raul;
+
+public class Constants {
+    public static class TokensCode{
+        public static final int PR_INT = 001;
+        public static final int PR_BOOL = 002;
+        public static final int PR_STRING = 003;
+        public static final int PR_IF = 101;
+        public static final int PR_ELSE = 102;
+        public static final int PR_INPUT = 103;
+        public static final int PR_LET = 104;
+        public static final int PR_FUN = 105;
+        public static final int PR_RET = 106;
+        public static final int PR_PRINT = 107;
+        public static final int IDENT = 201;
+        public static final int C_ENT = 202;
+        public static final int C_CAD = 203;
+        public static final int PCOMA = 801;
+        public static final int APARENTESIS = 802;
+        public static final int CPARENTESIS = 803;
+        public static final int COMA = 804;
+        public static final int ALLAVE = 805;
+        public static final int CLLAVE = 806;
+        public static final int ASIG = 811;
+        public static final int ASIGPLUS = 812;
+        public static final int OA_RESTA = 821;
+        public static final int OR_MENOR = 822;
+        public static final int OL_NEG = 823;
+    }
+}
