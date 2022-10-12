0d3ca88ea23f72f2d87d2de81924f0a0a62c5b1e
CASAMAYOR NAVAS, RAUL <raul.casamayor.navas@alumnos.upm.es>
2022-10-12 19:36:35 +0200

    Token attribute format correction

--- src/main/java/com/casamayor/raul/utils/TokenWriter.java
+++ src/main/java/com/casamayor/raul/utils/TokenWriter.java
@@ -8,7 +8,7 @@ import com.casamayor.raul.common.Token;
 /**
 * Object for writting tokens with the format specified in the project.
 * @author Raul Casamayor Navas
-* @version 1.0
+* @version 1.1
 * @since 12/10/2022
 */
 public class TokenWriter {
@@ -28,8 +28,16 @@ public class TokenWriter {
     
     public void write(Token t){
         String format = firstWrite ? TOKEN_FORMAT.substring(1) : TOKEN_FORMAT;
+        String attribute;
+        if(t.getAttribute() == null){
+            attribute = "";
+        }else if(t.getAttribute() instanceof String){
+            attribute = String.format("\"%s\"", t.getAttribute());
+        }else{
+            attribute = String.format("%d", t.getAttribute());
+        }
         try {
-            fw.write(String.format(format, t.getId(), t.getAttribute().toString()));
+            fw.write(String.format(format, t.getId(), attribute));
             firstWrite = false;
         } catch (Exception e) {
             System.err.printf("Error while printing a token (TYPE: %d, VALUE: %s)\n", t.getId(), t.getAttribute().toString());
